69ab0602fb0cec21e1fdd9dec9570197c1218306
VictorMaderaA <victor.madera.a@gmail.com>
2018-11-17 15:51:43 +0100

    arreglo de vistas

---
+++ Hack2ProgressAspMvc/Models/DataPoint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Hack2ProgressAspMvc.Models
+{
+    [DataContract]
+    public class DataPoint
+    {
+        public DataPoint(double x, double y)
+        {
+            this.x = x;
+            this.Y = y;
+        }
+
+        //Explicitly setting the name to be used while serializing to JSON.
+        [DataMember(Name = "x")]
+        public Nullable<double> x = null;
+
+        //Explicitly setting the name to be used while serializing to JSON.
+        [DataMember(Name = "y")]
+        public Nullable<double> Y = null;
+    }
+}
